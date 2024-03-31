dca952756efdc453f3d839c70e03f5a56be6cfcb
Shashwat Shekhar <119045090+shashwat22473@users.noreply.github.com>
2024-03-31 18:38:14 +0530

    Add files via upload

---
+++ Dump20240331 (4)/mydb_product.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `ID` int NOT NULL,
+  `Quantity` int NOT NULL,
+  `Name` varchar(45) NOT NULL COMMENT '\n\n',
+  `Price` int NOT NULL,
+  `Image` varchar(45) DEFAULT NULL,
+  `Description` varchar(45) DEFAULT NULL,
+  `Product_Category_id` int NOT NULL,
+  PRIMARY KEY (`ID`,`Product_Category_id`),
+  KEY `fk_Product_Product Category1_idx` (`Product_Category_id`),
+  CONSTRAINT `fk_Product_Product Category1` FOREIGN KEY (`Product_Category_id`) REFERENCES `product_category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,100,'Smartphone',500,NULL,NULL,1),(2,50,'T-shirt',20,NULL,NULL,2),(3,20,'Novel',10,NULL,NULL,3),(4,30,'Sofa',800,NULL,NULL,4),(5,40,'Action Figure',15,NULL,NULL,5),(6,60,'Refrigerator',12,NULL,NULL,6),(7,25,'Lipstick',30,NULL,NULL,7),(8,15,'Football',400,NULL,NULL,8),(9,35,'Necklace',50,NULL,NULL,9),(10,10,'Watch',100,NULL,NULL,10);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-31 18:36:27
