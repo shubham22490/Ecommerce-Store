30ec72a98cb5c8de4a41426cb800be4c4dadedfe
Shashwat Shekhar <119045090+shashwat22473@users.noreply.github.com>
2024-03-31 11:47:05 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,17 +1,35 @@
 import pymysql
-
+from datetime import datetime
+from fastapi import FASTAPI
 MYSQL_HOST = 'localhost'
-MYSQL_USER = 'your_username'
+MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'your_password'
-MYSQL_DB = 'your_database_name'
+MYSQL_DB = 'mydb'
 
-conn = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER, password=MYSQL_PASSWORD, database=MYSQL_DB)
-cursor = conn.cursor()
-
-
+# show all different categories
+@app.get("/show_product_categories")
 def get_categories() -> list[dict]:
+    conn=mysql.connector.connect(host=MYSQL_HOST,username=MYSQL_USER,password=MYSQL_PASSWORD,database=MYSQL_DB)
+    my_cursor=conn.cursor()
 
-    return
+    # Fetch column names for the product_category table
+    table_name = 'product_category'  
+    my_cursor.execute(f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'")
+    column_names = my_cursor.fetchall()
+    column_names = [col[0] for col in column_names]
+    # Fetch records from the product_category table
+    my_cursor.execute("SELECT * FROM product_category")
+    records = my_cursor.fetchall()
+    # Construct dictionary with column names as keys and record values as values
+    result = []
+    for record in records:
+        record_dict = {}
+        for j in range(len(column_names)):
+            record_dict[column_names[j]]=record[j]
+        result.append(record_dict)
+
+    conn.close()
+    return result
 
 
 def get_products(category_id: int) -> list[dict]:
