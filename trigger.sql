69c28cce69baeead2c815c5ca9f051073a74326e
Shashwat Shekhar <119045090+shashwat22473@users.noreply.github.com>
2024-04-25 19:01:10 +0530

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,42 +1,61 @@
 DELIMITER $$
 
-CREATE TRIGGER after_product_update
-AFTER UPDATE ON product
+CREATE TRIGGER UpdateAllAgesAfterInsert
+AFTER INSERT ON Customer
 FOR EACH ROW
 BEGIN
-    IF NEW.Quantity = 0 THEN
-        DELETE FROM product WHERE ID = NEW.ID;
-    END IF;
+    -- Updating age for all customers based on their DOB
+    UPDATE Customer
+    SET Age = TIMESTAMPDIFF(YEAR, DOB, CURDATE());
 END$$
 
 DELIMITER ;
+
+
+ DELIMITER $$
+
+
+-- trigger updates the quantity of the item 
+CREATE TRIGGER AfterPurchaseUpdateQuantity
+AFTER INSERT ON mydb.order_history
+FOR EACH ROW
+BEGIN
+  UPDATE mydb.Product
+  SET Quantity = Quantity - NEW.Quantity
+  WHERE ID = NEW.Product_id;
+END$$
+
+DELIMITER ;
+
+-- trigger checks the quantity available before placing order
+DELIMITER $$
+
+CREATE TRIGGER BeforeAddToCart
+BEFORE INSERT ON mydb.order_history
+FOR EACH ROW
+BEGIN
+  DECLARE available_quantity INT;
+  SELECT Quantity INTO available_quantity FROM mydb.Product WHERE ID = NEW.Product_ID;
+  IF available_quantity < NEW.Quantity THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Insufficient stock for the requested product.';
+  END IF;
+END$$
+
+DELIMITER ;
+
+
+-- trigger removes the data of a customer
 
 DELIMITER $$
 
-CREATE TRIGGER increment_cart_quantity
-AFTER INSERT ON Cart
+CREATE TRIGGER CleanupAfterCustomerDeletion
+AFTER DELETE ON mydb.Customer
 FOR EACH ROW
 BEGIN
-    UPDATE Customer
-    SET Cart_quantity = Cart_quantity + NEW.Quantity
-    WHERE Cart_Id = NEW.Cart_Id;
+  DELETE FROM mydb.Cart WHERE Phone_Number = OLD.Phone_Number;
+  DELETE FROM mydb.Address WHERE Customer_Phone_Number = OLD.Phone_Number;
+  DELETE FROM mydb.Selects WHERE Customer_Phone_Number = OLD.Phone_Number;
 END$$
 
 DELIMITER ;
-
-
-DELIMITER $$
-
-CREATE TRIGGER decrement_cart_quantity
-AFTER DELETE ON Cart
-FOR EACH ROW
-BEGIN
-    UPDATE Customer
-    SET Cart_quantity = Cart_quantity - OLD.Quantity
-    WHERE Cart_Id = OLD.Cart_Id;
-END$$
-
-DELIMITER ;
-
-
-
